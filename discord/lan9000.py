50b00ab4c48bead78a4c746aa0c321e2950a1638
Jesse <jesseforney8@gmail.com>
2024-04-27 22:23:14 -0500

    pokemon update

--- discord/lan9000.py
+++ discord/lan9000.py
@@ -4,6 +4,38 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import random
+from datetime import datetime
+import pokebase as pd
+
+
+def pokedex_lookup(name):
+
+    try:
+    
+        pokemon_ = pd.pokemon(name)
+
+        pokemon_type = ""
+        p_type_list = []
+
+        
+        for t in pokemon_.types:
+            
+            p_type_list.append(str(t.type))
+            pokemon_type = ", ".join(p_type_list)
+            pokemon_type += "."
+
+        
+        pokemon_height = pokemon_.height
+        name = str(name).capitalize()
+
+        return f"Name: {name}\nElement Type: {pokemon_type}\nHeight: {pokemon_height}"
+    
+    except:
+        return "Could not find Pokemon. :("
+
+
+
+
 
 TOKEN = os.environ.get('TOKEN')
 load_dotenv()
@@ -57,6 +89,29 @@ async def on_message(message):
 
         msg = random.choice(poop_facts)
         await message.channel.send(msg)
+    if message.content.startswith("!time"):
+        now = datetime.now()
+        if now.hour < 12:
+            hour = now.hour
+            day_night = "AM"
+        else:
+            hour = now.hour - 12
+            day_night = "PM"
+
+        current_time = f"{hour}:{now.minute}{day_night}"
+        await message.channel.send(current_time)
+
+    if message.content.startswith('!pokemon'):
+        pokemon_name = message.content.split(" ")[-1]
+        
+        await message.channel.send(pokedex_lookup(pokemon_name))
+
+
+
+
+
+
+
 
     #roll dice function
     
